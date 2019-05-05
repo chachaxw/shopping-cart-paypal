89dde16e688cb9dd61fb3c2327c8eb8056b2b7de
chacha <867571123@qq.com>
2019-05-05 13:17:13 +0800

    Update index.pug and main.js

--- public/js/main.js
+++ public/js/main.js
@@ -70,4 +70,43 @@ $(document).ready(function() {
   $(document).on('click', '.closeBtn', function () {
     $('#exitpopup-overlay').hide();
   });
+
+  var person = [
+    ["Bojan Mehle", "Ljubljana", "x1"],
+    ['Sonya georgalis', "Red hikl, ACT",'x1'],
+    ['Jancele Jump', "Grants Pass, OR",'x2'],
+    ['Ginellie Uribe', "Oceanside, CA",'x2'],
+    ['Adriana Montesinos', "Lawrence, KS",'x3'],
+    ['Edith Diaz', "chicago, IL",'x3'],
+    ['Mary thompson', "coxs creek, KY",'x1'],
+    ['Thomas Kenney', "Fullerton, CA",'x2'],
+    ['Suze Benson', "Seabrook, TX",'x2'],
+    ['Lucinda Rave', "Glendale, AZ",'x3'],
+    ['Monica Fishwick', "Sheel Cove, NSW",'x1'],
+    ['Terri liesman', "Hampton, GA",'x1'],
+    ['teresa duke', "OWENSBORO, KY",'x1']
+  ];
+
+  chgValue();
+
+  setInterval(function(){
+    $(".custom-social-proof").stop().slideToggle('slow', function () {
+      if ($(this).is(':visible')) {
+        setTimeout(function(){
+          chgValue();
+        }, 3500);
+      }
+    });
+  }, 3000);
+
+  function chgValue(){
+    var rand = person[Math.floor(Math.random() * person.length)];
+    $('#purchased_person').text(rand[0]);
+    $('#purchased_country').text(rand[1]);
+    $('#purchased_qty').text(rand[2]);
+  }
+
+  $(".custom-close").click(function() {
+    $(".custom-social-proof").stop().slideToggle('slow');
+  });
 });
